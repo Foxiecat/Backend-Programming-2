d4cec693dced9ede2d7b912a9ca8110172306c83
Danica Ellefsen Kvilhaug <65165280+Foxiecat@users.noreply.github.com>
2024-08-22 23:13:30 +0200

    Finished(?) Node class

--- Assignments/AM-Linked_List/AM-Linked_List/Models/Node.cs
+++ Assignments/AM-Linked_List/AM-Linked_List/Models/Node.cs
@@ -1,6 +1,7 @@
 namespace AM_Linked_List.Models;
 
-public class Node
+public class Node(string value, Node next)
 {
-    
+    private string Value { get; set; } = value;
+    private Node Next { get; set; } = next;
 }
