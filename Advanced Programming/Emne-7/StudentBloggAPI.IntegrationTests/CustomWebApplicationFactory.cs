79a5ed4fd6c541237c7246cd7c46d4696d58d66e
Danica Ellefsen Kvilhaug <danicaglox@gmail.com>
2024-10-23 11:24:20 +0200

    More Week 43

---
+++ Advanced Programming/Emne-7/StudentBloggAPI.IntegrationTests/CustomWebApplicationFactory.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using StudentBloggAPI.Features.Users.Interfaces;
+
+namespace StudentBloggAPI.IntegrationTests;
+
+public class CustomWebApplicationFactory : WebApplicationFactory<Program>
+{
+    public Mock<IUserRepository> UserRepositoryMock { get; set; } = new();
+
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        base.ConfigureWebHost(builder);
+
+        builder.ConfigureTestServices(services =>
+        {
+            services.AddSingleton(UserRepositoryMock.Object);
+        });
+    }
+}
--- Advanced Programming/Emne-7/StudentBloggAPI/obj/Debug/net8.0/StudentBloggAPI.AssemblyInfo.cs
+++ Advanced Programming/Emne-7/StudentBloggAPI/obj/Debug/net8.0/StudentBloggAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentBloggAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1961389c0d1a526686e3513eec5d45c8a9e377b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentBloggAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentBloggAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
